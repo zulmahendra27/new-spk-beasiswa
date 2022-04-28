22d1a56fb7c3f55d8c5bf92949f205ef13c2e28a
Zulmahendra <zulmahendra27@gmail.com>
2022-04-28 13:06:22 +0700

    Update db

--- db_spk.sql
+++ db_spk.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.1.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 21, 2022 at 07:58 AM
+-- Generation Time: Apr 28, 2022 at 06:05 AM
 -- Server version: 5.7.33
 -- PHP Version: 7.4.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -21,6 +20,66 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_spk`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kriteria`
+--
+
+CREATE TABLE `kriteria` (
+  `id_kriteria` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `bobot` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kriteria`
+--
+
+INSERT INTO `kriteria` (`id_kriteria`, `nama`, `bobot`) VALUES
+(1, 'IPK', 40),
+(2, 'Penghasilan Orang Tua', 30),
+(3, 'Jumlah Saudara', 10),
+(4, 'Semester', 20);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mhs`
+--
+
+CREATE TABLE `mhs` (
+  `id_mhs` int(11) NOT NULL,
+  `nim` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `prodi` varchar(150) NOT NULL,
+  `nohp` varchar(13) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mhs`
+--
+
+INSERT INTO `mhs` (`id_mhs`, `nim`, `nama`, `tgl_lahir`, `prodi`, `nohp`, `email`, `alamat`) VALUES
+(1, 12, 'as', '2022-04-13', 'as', '123', 'ayam@gmail.com', 'Jalan Hang Tuah'),
+(3, 124, 'Saya', '2022-04-06', 'as', '082369940750', 'melda@gmail.com', 'Aceh Tamiang');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seleksi`
+--
+
+CREATE TABLE `seleksi` (
+  `id_seleksi` int(11) NOT NULL,
+  `id_mhs` int(11) NOT NULL,
+  `id_kriteria` int(11) NOT NULL,
+  `nilai` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -47,6 +106,25 @@ INSERT INTO `user` (`id_user`, `username`, `password`, `level`) VALUES
 --
 
 --
+-- Indexes for table `kriteria`
+--
+ALTER TABLE `kriteria`
+  ADD PRIMARY KEY (`id_kriteria`);
+
+--
+-- Indexes for table `mhs`
+--
+ALTER TABLE `mhs`
+  ADD PRIMARY KEY (`id_mhs`),
+  ADD UNIQUE KEY `nim` (`nim`);
+
+--
+-- Indexes for table `seleksi`
+--
+ALTER TABLE `seleksi`
+  ADD PRIMARY KEY (`id_seleksi`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -55,6 +133,24 @@ ALTER TABLE `user`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `kriteria`
+--
+ALTER TABLE `kriteria`
+  MODIFY `id_kriteria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `mhs`
+--
+ALTER TABLE `mhs`
+  MODIFY `id_mhs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `seleksi`
+--
+ALTER TABLE `seleksi`
+  MODIFY `id_seleksi` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
